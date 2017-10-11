6e06781c08cd63bd18f11a3218af25756cac4124
aitehappiness <songq91@gmail.com>
2017-10-11 18:45:10 +0800

    init-detect

---
+++ detect-init.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import json, sys
+
+
+def gen(url):
+    flow = {
+        "url": url,
+        "ops": "download()",
+        "type": "image",
+        "label": {"detect": {"general_d": {"bbox": []}}},
+        "source_url": ""
+    }
+    return flow
+
+
+if __name__ == '__main__':
+    name = sys.argv[1] if len(sys.argv) > 1 else 'in.txt'
+    urllist = []
+    with open(name + '.json', 'w+') as json_content:
+        with open(name) as content:
+            line = content.readline()
+            while line:
+                url = line.rstrip('\n')
+                urllist.append(url)
+                line = content.readline()
+
+    r = '\n'.join([json.dumps(gen(url)) for url in urllist])
+    with open(name+'.json', 'w+') as f: f.write(r)
